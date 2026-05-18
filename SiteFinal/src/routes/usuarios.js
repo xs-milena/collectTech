61b27367cc91a53a8e18d01f41c6054dd6608a07
Thabata356 <thabatav7@gmail.com>
2026-05-18 14:10:08 -0300

    :sparkles: Feat: Colocar o html e css do gerenciar usuario e começar o js.

--- SiteFinal/src/routes/usuarios.js
+++ SiteFinal/src/routes/usuarios.js
@@ -12,4 +12,8 @@ router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
 });
 
+router.get("/buscarDadosPorNome", function (req, res) {
+    usuarioController.buscarDados(req, res);
+});
+
 module.exports = router;
