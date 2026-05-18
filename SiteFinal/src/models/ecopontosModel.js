f05cdd0777867ac4766adfbb444db348e6f6e80a
Julia-Macedo-Tech <julia.macedo@sptech.school>
2026-05-18 13:57:47 -0300

    listarBairro funcionando

--- SiteFinal/src/models/ecopontosModel.js
+++ SiteFinal/src/models/ecopontosModel.js
@@ -36,9 +36,42 @@ function listarEcoponto(id_empresa) {
 
 function listarBairro(id_empresa) {
   var instrucaoSql = `
-  select id_empresa, nome_subprefeitura, soma_nivel_cheia, soma_nivel_medio, soma_nivel_baixo from vw_bairros_empresa
-  where id_empresa = ${id_empresa};
+  select * from vw_bairros_empresa
+where id_empresa = 1;
   `;
+
+//   create view vw_bairros_empresa
+// as
+// select e.id_empresa, s.nome as nome_subprefeitura, eco.bairro,
+// (sum(case
+// when v.nivel_preenchimento > 80 then v.nivel_preenchimento
+// else 0
+// end) / count(case
+// when v.nivel_preenchimento > 80 then v.nivel_preenchimento
+// else 0
+// end)) 
+// as soma_nivel_cheia,
+// (sum(case
+// when v.nivel_preenchimento > 50 and v.nivel_preenchimento <= 80 then v.nivel_preenchimento
+// else 0
+// end) / count( case
+// when v.nivel_preenchimento > 50 and v.nivel_preenchimento <= 80 then v.nivel_preenchimento
+// else 0
+// end))
+// as soma_nivel_medio,
+// (sum(case
+// when v.nivel_preenchimento <= 50 then v.nivel_preenchimento
+// else 0
+// end) / count(case
+// when v.nivel_preenchimento <= 50 then v.nivel_preenchimento
+// else 0
+// end))
+// as soma_nivel_baixo
+// from vw_nivel_lixeiras_empresa v
+// join empresa e on e.id_empresa = v.id_empresa 
+// join ecoponto eco on v.id_ecoponto = eco.id_ecoponto
+// join subprefeitura s on eco.fk_subprefeitura = s.id_subprefeitura 
+// GROUP BY e.id_empresa, s.nome, eco.bairro;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
